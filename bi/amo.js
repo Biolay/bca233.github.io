b5558cedaaac7430518793705ed279ddf562e75f
Biolay <maxbian_2023@qq.com>
2024-08-14 04:19:51 +0800

    amo.js

--- bi/amo.js
+++ bi/amo.js
@@ -11,9 +11,9 @@ myImage.onclick = function() {
 function 测试() { alert('都叫你别点这里了。')}
 //人机验证
 function RJ储存() {let blend = prompt('请问您是人类吗？');
-if (blend = 是) { localStorage.setItem('name',blend);
-  console.log(blend)
-} else { RJ储存();
+if (!blend = 是 ) { RJ储存();
+} else { localStorage.setItem('name',blend);
+  console.log(blend);
   
 }
 }
