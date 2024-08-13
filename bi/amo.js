cf7628bc69df488e1e3257d160aeb73b38a36828
Biolay <maxbian_2023@qq.com>
2024-08-13 21:56:32 +0800

    Update amo.js

--- bi/amo.js
+++ bi/amo.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if (mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
+  if (mySrc === 'https://s21.ax1x.com/2024/08/13/pApyKeg.jpg') {
+    myImage.setAttribute ('src','https://s2.loli.net/2024/08/13/uemLP64qgfpzVKI.webp');
   } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
+    myImage.setAttribute ('src','https://s21.ax1x.com/2024/08/13/pApyKeg.jpg');
   }
 };
