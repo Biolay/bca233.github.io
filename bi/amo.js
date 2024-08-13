babb819c864934d7d74a7eab2a598cb9b47b11fa
Biolay <maxbian_2023@qq.com>
2024-08-13 21:23:30 +0800

    amo.js

--- bi/amo.js
+++ bi/amo.js
@@ -1,6 +1,6 @@
 let myImage = document.querySelector("img");
 myImage.onclick = function () {
-let mySrc=myImage. getAttribute(“Src”)；
+let mySrc=myImage. getAttribute(“src”)；
 if (mySrc === "https://s21.ax1x.com/2024/08/13/pApyKeg.jpg")
 {
 myImage.setAttribute("src", "https://s2.loli.net/2024/08/13/uemLP64qgfpzVKI.webp");
